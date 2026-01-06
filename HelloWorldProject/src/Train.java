b6938aeeae9753f4b852660103095ab4c8caa436
Preston Havenstein <meadehavenstein@gmail.com>
2026-01-06 17:48:23 +0000

    check 1 - 1/6

--- HelloWorldProject/src/Train.java
+++ HelloWorldProject/src/Train.java
@@ -1,6 +1,50 @@
 public class Train {
     private String name, product, origin, destination;
     private int weight, miles;
+    public train(String cn, String dc)
+    {
+	    String name = cn;
+    	String to = dc;
+    }
+    public train(String cn, String c, String oc, String dc, int w, int mt)
+    {
+	    String name = cn;
+    	String contents = c;
+    	String from = oc;
+    	String to = dc;
+    	int weight = w;
+    	int miles = mt;
+    }
+
+    public void getName()
+    {
+    	return name;
+    }
+
+    public void getContents()
+    {
+    	return contents;
+    }
+
+    public void getOrigins()
+    {
+    	return from;
+    }
+
+    public void getDestination()
+    {
+    	return to;
+    }
+
+    public void getWeight()
+    {
+    	return weight;
+    }
+
+    public void getMiles()
+    {
+    	return miles;
+    }
 
 
 }
